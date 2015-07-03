49ff896d294f29f6f457c4cd51c4553720a19796
Gary Iams <ge.iams@gmail.com>
2015-07-03 16:11:43 -0400

    Setup WebPage model and tests
    Will persist a crawled page's linked pages and static assets.

---
+++ lib/website_struct/web_page.rb
@@ -0,0 +1,6 @@
+require "active_record"
+
+module WebsiteStruct
+  class WebPage < ActiveRecord::Base
+  end
+end
---
+++ spec/website_struct/web_page_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+require "website_struct/web_page"
+
+describe WebsiteStruct::WebPage do
+  xit "persists a crawler page" do
+  end
+end
